ddb3521badd79c78d0b6d2a3cc3ee7e9bc4b0205
Brett Wilson <drytoastman@gmail.com>
2018-05-16 19:57:11 -0700

    start of work to divide into cgi as well as simple httpd for proxy through nginx (fastcgi is too complex for our needs)

--- src/redirector.lua
+++ src/redirector.lua
@@ -10,58 +10,9 @@ function readstate()
     return obj
 end
 
-function service2link(addr, service)
-    if service == 'REGISTRATION' then
-        return string.format("http://%s/register", addr)
-    end
-    return string.format("http://%s/results", addr)
-end
-
-function service2name(service)
-    if service == 'REGISTRATION' then
-        return 'Registration'
-    elseif service == 'DATAENTRY' then
-        return 'Data Entry'
-    else
-        return 'Database'
-    end
-end
-
-function redirect(state, service, target)
-    for sid, entry in pairs(state) do
-        if entry.services[service] ~= nil then
-            io.write("Status: 302 Found\r\n")
-            io.write(string.format("Location: http://%s/%s\r\n\r\n", entry.addr, target))
-            os.exit(0)
-        end
-    end
-end
-
-function servererror(msg)
-    io.write("Status: 500 Internal Server Error\r\n")
-    io.write("Content-Type: text/plain\r\n\r\n")
-    io.write(msg)
-    os.exit(0)
-end
-
-
-function main_process()
-    local host  = os.getenv("HTTP_HOST")
-    local uri   = os.getenv("REQUEST_URI")
-    local state = readstate()
-
-    if (string.find(host, 'results')) or (string.find(uri, 'results')) then
-        redirect(state, 'DATAENTRY', 'results')
-    end
-
-    if (string.find(host, 'register')) or (string.find(uri, 'register')) then
-        redirect(state, 'REGISTRATION', 'register')
-    end
-
-
-    io.write("Status: 200 OK\r\n")
-    io.write("Content-Type: text/html\r\n\r\n")
-    io.write([[
+function resulthtml(state)
+    ret = {""}
+    table.insert(ret, [[
     <!DOCTYPE html>
     <html lang="en">
     <head>
@@ -112,26 +63,110 @@ function main_process()
     ]])
     for sid, entry in pairs(state) do
         if entry.services.DATABASE then
-            io.write("<div class='server'>\n")
-            io.write(string.format("<div class='addr'>%s</div>\n", entry.addr))
-            io.write(string.format("<div class='service %s'><a href='http://%s/results'>Results</a></div>\n",
-                    entry.services.DATAENTRY and "active" or "", entry.addr))
-            io.write(string.format("<div class='service %s'><a href='http://%s/register'>Register</a></div>\n",
-                    entry.services.REGISTRATION and "active" or "", entry.addr))
-            io.write(string.format("<div class='service'><a href='http://%s/admin'>Admin</a></div>\n",
+            table.insert(ret, "<div class='server'>\n")
+            table.insert(ret, string.format("<div class='addr'>%s</div>\n",
                     entry.addr))
-            io.write("</div>\n")
+            table.insert(ret, string.format("<div class='service %s'><a href='http://%s/results'>Results</a></div>\n",
+                    entry.services.DATAENTRY and "active" or "", entry.addr))
+            table.insert(ret, string.format("<div class='service %s'><a href='http://%s/register'>Register</a></div>\n",
+                    entry.services.REGISTRATION and "active" or "", entry.addr))
+            table.insert(ret, string.format("<div class='service'><a href='http://%s/admin'>Admin</a></div>\n",
+                    entry.addr))
+            table.insert(ret, "</div>\n")
         end
     end
-    io.write([[
-    </body>
-    </html>
-    ]])
+    table.insert(ret, "</body></html>")
+    return table.concat(ret, "")
 end
 
 
-local ok, msg = pcall(main_process)
-if not ok then
-    servererror(msg)
+function should_redirect(state, service, target)
+    for sid, entry in pairs(state) do
+        if entry.services[service] ~= nil then
+            return string.format("http://%s/%s\r\n\r\n", entry.addr, target)
+        end
+    end
+end
+
+function find_redirect(state, host, uri)
+    local redirectto = nil
+
+    if (string.find(host, 'register')) or (string.find(uri, 'register')) then
+        redirectto = should_redirect(state, 'REGISTRATION', 'register')
+    end
+
+    if (string.find(host, 'results')) or (string.find(uri, 'results')) then
+        redirectto = should_redirect(state, 'DATAENTRY', 'results')
+    end
+
+    return redirectto
+end
+
+
+function cgi_process()
+    local host  = os.getenv("HTTP_HOST")
+    local uri   = os.getenv("REQUEST_URI")
+    local state = readstate()
+    local redirect = find_redirect(state, host, uri)
+
+    if redirect ~= nil then
+        io.write("Status: 302 Found\r\n")
+        io.write("Location: "..redirect.."\r\n\r\n")
+    else
+        io.write("Status: 200 OK\r\n")
+        io.write("Content-Type: text/html\r\n\r\n")
+        io.write(resulthtml(state))
+    end
+end
+
+
+function httpd_process()
+    local socket = require ("socket")
+
+    sock, err = socket.tcp4()
+    sock:setoption('reuseaddr', true)
+    res, err = sock:bind('127.0.0.1', 9000)
+    res, err = sock:listen()
+
+    while 1 do
+        local client = sock:accept()
+        client:settimeout(5)
+
+        while 1 do
+            local line,err = client:receive("*l")
+            if line == nil or line == "\r\n" or line == "" then break end
+            print(line)
+        end
+
+        local host  = "nothing" --somethingfromheaders
+        local uri   = "nothing" --somethingfromheaders
+        local state = readstate()
+        local redirect = find_redirect(state, host, uri)
+
+        if redirect ~= nil then
+            client:send("HTTP/1.1 302 Found\r\n")
+            client:send("Location: "..redirect.."\r\n\r\n")
+        else
+            client:send("HTTP/1.1 200 OK\r\n")
+            client:send("Content-Type: text/html\r\n\r\n")
+            client:send(resulthtml(state))
+        end
+
+        client:close()
+    end
+end
+
+
+if os.getenv("REQUEST_URI") == nil then
+    -- Assuming we are started as a daemon for proxying, way easier to implement vs FastGCI
+    httpd_process()
+else
+    -- Assuming we are started as a cgi script
+    local ok, msg = pcall(cgi_process)
+    if not ok then
+        io.write("Status: 500 Internal Server Error\r\n")
+        io.write("Content-Type: text/plain\r\n\r\n")
+        io.write(msg)
+    end
 end
 
